6e916901c3a06aa63786f0b585d380f3b7253948
Manisha Priyadarshini <manishapriya94@github.com>
2020-06-03 00:41:45 -0700

    Update index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,6 @@
 const auth = require("./auth");
 const tutorial = require("./tutorial");
 const home = require("./home");
+const donate = require("./donate");
 
-module.exports = { auth, tutorial, home };
+module.exports = { auth, tutorial, home, donate };
